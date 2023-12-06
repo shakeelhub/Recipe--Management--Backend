a8c6eed8e3bc5b6ab2bf4025194df58c18cdcee7
MD Shakeel S <135600794+shakeelhub@users.noreply.github.com>
2023-12-07 02:53:17 +0530

    commit

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const config = require("./db/config");
 const Home = require("./controllers/controller");
 const LoginRoute = require("./routes/LoginRoute");
 const RegisterRoute = require("./routes/RegisterRoute");
-const verifyToken = require("./Middleware/middleware");
 const RecipeRoute = require("./routes/RecipeRoute");
 const ForgotPassword = require("./routes/forgotPassword");
 
@@ -26,7 +25,7 @@ app.use("/auth", RecipeRoute);
 app.use("/auth", router);
 app.use("/auth", ForgotPassword);
 
-router.get("/", verifyToken, Home.Home);
+router.get("/", Home.Home);
 
 module.exports = router;
 
